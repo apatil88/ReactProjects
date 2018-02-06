40da824c40db554c23f90803e780ec91f57b0250
apatil88 <amrut.patil88@hotmail.com>
2018-02-06 07:25:49 -0500

    Added video list component

---
+++ ReactWebApp/src/components/video_list.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import VideoListItem from './video_list_item'
+
+//props arrive as arguments to Functional component
+const VideoList = (props) => {
+
+    const videoItems = props.videos.map((video) => {
+        return <VideoListItem key= { video.etag } video={ video } />  //key property to uniquely identify each element in the list
+    });
+
+    return (
+
+        <ul className="col-md-4 list-group">
+            { videoItems }
+        </ul>
+    )
+}
+
+export default VideoList;
