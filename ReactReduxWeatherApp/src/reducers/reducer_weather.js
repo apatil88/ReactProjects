3a06dcbdba7f2f967e0d6e3e22853d7f4bed1e72
apatil88 <amrut.patil88@hotmail.com>
2018-02-24 09:35:21 -0500

    Preventing mutation of application state and displaying data from older and newer city search

--- ReactReduxWeatherApp/src/reducers/reducer_weather.js
+++ ReactReduxWeatherApp/src/reducers/reducer_weather.js
@@ -1,4 +1,12 @@
-export default function(state = null, action){
-    console.log('Action received : ' , action);  //Redux-promise middleware creates a new action with the actual payload data that is sent to all reducers.
+import { FETCH_WEATHER } from '../actions/index'
+
+export default function(state = [], action){
+    //console.log('Action received : ' , action);  //Redux-promise middleware creates a new action with the actual payload data that is sent to all reducers.
+    
+    switch(action.type){
+        case FETCH_WEATHER:
+            //return state.concat([ action.payload.data ]);  //Prevent mutation of state. Return a new version of state with older city search data and new city search data
+            return [ action.payload.data, ...state ];
+        }
     return state;
 }
