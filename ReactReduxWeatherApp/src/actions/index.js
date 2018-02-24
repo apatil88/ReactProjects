40a7c7e308a7219e2509c9d242df9558f5d37575
apatil88 <amrut.patil88@hotmail.com>
2018-02-24 09:32:25 -0500

    Commented code

--- ReactReduxWeatherApp/src/actions/index.js
+++ ReactReduxWeatherApp/src/actions/index.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-const API_KEY  = 'YOUR_API_KEY'
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}` 
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
@@ -9,7 +8,7 @@ export function fetchWeather(city){
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);  //AJAX request using Axios library. Returns a promise.
 
-    console.log('Request : ', request);
+    //console.log('Request : ', request);
     
     return {
         type : FETCH_WEATHER,
