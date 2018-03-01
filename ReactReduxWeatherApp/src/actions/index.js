caec246c8197ebc49476c064267155a7dae51300
apatil88 <amrut.patil88@hotmail.com>
2018-02-28 20:16:41 -0500

    API KEY

--- ReactReduxWeatherApp/src/actions/index.js
+++ ReactReduxWeatherApp/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+const API_KEY  = 'c60596606b9c7eb8c6ac2dae9bf89f9c'
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}` 
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
