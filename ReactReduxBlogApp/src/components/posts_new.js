cb779d6f7c4ff5c4b8e8da24f8965b150e81a568
apatil88 <amrut.patil88@hotmail.com>
2018-03-05 07:34:11 -0500

    Adding Field component to PostsNew Redux Form

--- ReactReduxBlogApp/src/components/posts_new.js
+++ ReactReduxBlogApp/src/components/posts_new.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component{
-    render(){
+
+    renderTitleField(field){  //field contains event handlers that we need to wire up JSX that we return
         return (
             <div>
-                PostsNew
+                <input 
+                    type="text"
+                    { ...field.input } //communicate built in event handlers as props to Field component
+                />
             </div>
+        );
+    }
+
+    render(){
+        return (
+            <form>
+                <Field 
+                    name="title"
+                    component={this.renderTitleField}
+                />
+            </form>
         );
     }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form : 'PostsNewForm' //needs to be unique if we do not want this form to share its application state with any other forms
+})(PostsNew);
