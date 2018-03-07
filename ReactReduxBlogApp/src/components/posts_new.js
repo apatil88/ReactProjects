934c847818b91e02cd7e0d567c8882e64b850809
apatil88 <amrut.patil88@hotmail.com>
2018-03-07 07:13:59 -0500

    Updating handling form submittal. Adding styling to error messages and inputs.

--- ReactReduxBlogApp/src/components/posts_new.js
+++ ReactReduxBlogApp/src/components/posts_new.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component{
 
     renderField(field){  //field contains event handlers that we need to wire up JSX that we return
+        
+        const {meta : {touched, error}} = field;  //destructuring nested properties
+        const className = `form-group ${ touched && error ? 'has-danger' : ''}`;
+
         return (
-            <div className="form-group">
+            <div className={ className }>
                 <label>{ field.label }</label>
                 <input className="form-control"
                     type="text"
                     { ...field.input } //communicate built in event handlers as props to Field component
                 />
-                {field.meta.error}
+                <div className="text-help">
+                    { touched ? error : ''}
+                </div>
             </div>
         );
     }
 
+    onSubmit(values){
+        //this === component
+        console.log(values);
+    }
     
     render(){
+        const { handleSubmit } = this.props;  //handleSubmit is a property that is passed along by redux form
+
+        //handleSubmit handles the redux side of things (for instance form validation)
+        //If validation is appropriate, the callback function is invoked to actually take the data and submit it
         return (
-            <form>
+            <form onSubmit={ handleSubmit(this.onSubmit.bind(this)) } >
                 <Field 
                     label="Title"
                     name="title"
@@ -35,6 +50,9 @@ class PostsNew extends Component{
                     name="content"
                     component={this.renderField}
                 />
+
+                <button type="submit" className="btn btn-primary"> Save </button>
+                <Link to="/" className="btn btn-danger"> Cancel </Link>
             </form>
         );
     }
