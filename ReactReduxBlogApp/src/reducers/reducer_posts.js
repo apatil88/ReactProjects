0b440324593fadb524f536d6f6792fc5a287b7ae
apatil88 <amrut.patil88@hotmail.com>
2018-03-10 10:10:02 -0500

    Importing delete post action creator in post reducer

--- ReactReduxBlogApp/src/reducers/reducer_posts.js
+++ ReactReduxBlogApp/src/reducers/reducer_posts.js
@@ -1,13 +1,16 @@
 import _ from 'lodash'
-import { FETCH_POSTS, FETCH_POST } from '../actions/index'
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions/index'
 
 export default function (state = {}, action){
     switch(action.type){
+        case DELETE_POST:
+            return _.omit(state, action.payload); //If the state object has a key of post id, omit that particular post and return a new instance
+
         case FETCH_POSTS:
-            return _.mapKeys(action.payload.data, 'id');     //Returns an object with key as the post id and the value as the post object
+            return _.mapKeys(action.payload.data, 'id');     //Returns an object with key as the post id and the value as the post object. Treat application state as an object.
         
         case FETCH_POST:
-            return { ...state, [action.payload.data.id] : action.payload.data} //Add the particular fetched post to the state
+            return { ...state, [action.payload.data.id] : action.payload.data} //Fetching the same post again overwrites the existing state since state is an object
 
         default:
             return state;
