5f70849f7a230fb21029f1c36faaec4f47073073
apatil88 <amrut.patil88@hotmail.com>
2018-04-17 07:11:12 -0400

    React-Middlewares

---
+++ React-Middlewares/src/actions/index.js
@@ -0,0 +1,13 @@
+import { FETCH_USERS } from './types';
+
+export function fetchUsers(){
+    return {
+        type : FETCH_USERS,
+        payload : [
+            { name : 'John'},
+            { name : 'Jane'},
+            { name : 'Jim'}
+
+        ]
+    }
+}
