66b3ffdbc3de7d69fd008d5e31897e9dc4759e44
apatil88 <amrut.patil88@hotmail.com>
2018-04-22 19:13:48 -0400

    Node.js API server for authentication

---
+++ auth-server/index.js
@@ -0,0 +1,16 @@
+//Main starting point of the application
+const express    = require('express');
+const http       = require('http');
+const morgan     = require('morgan');
+const bodyParser = require('body-parser'); 
+const app        = express();
+
+//App setup
+
+
+//Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+app.listen(port);
+
+console.log('Server listening on port: ', port);
