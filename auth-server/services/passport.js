d7a1f38c30e446672dfedbf8d87f18ad054e26bf
apatil88 <amrut.patil88@hotmail.com>
2018-05-10 20:25:36 -0400

    Setting up Passport local strategy for signing in a user

--- auth-server/services/passport.js
+++ auth-server/services/passport.js
@@ -3,6 +3,37 @@ const User     = require('../models/user');
 const config   = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');  //Strategy for authentication using email and password
+
+//Create a Local Strategy
+const localOptions = { usernameField: 'email'}; //When username is needed, look at the email property in the request
+const localLogin = new LocalStrategy(localOptions, function(email, password, done){
+
+    //Verify this username and password, call done with the user if the username and password is correct, otherwise call done
+
+    User.findOne({email: email}, function(err, user){
+        if(err){
+            return done(err);
+        }
+
+        if(!user){
+            return done(null, false);   //User was not found
+        }
+
+        //compare passwords - if 'password' equals user.password
+        user.comparePassword(password, function(err, isMatch){
+            if(err){
+                return done(err);
+            }
+
+            if(!isMatch){
+                return done(null, false);
+            }
+
+            return done(null, user);
+        })
+    })
+});
 
 //Set up options for JwtStrategy
 const jwtOptions = {
@@ -32,5 +63,6 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done){
 })
 
 
-//Tell passport to use this Strategy
+//Tell passport to use Strategies
 passport.use(jwtLogin);
+passport.use(localLogin);
