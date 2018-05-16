a5b6391cb0c965710cd46e22ce133441ec50a8ee
apatil88 <amrut.patil88@hotmail.com>
2018-05-16 07:18:43 -0400

    Adding reducer to maintain authenticated state

--- React-client/src/reducers/index.js
+++ React-client/src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
+import authReducer from './auth_reducer';
+import auth_reducer from './auth_reducer';
 
 const rootReducer = combineReducers({
-  form: form
+  form: form,
+  auth: auth_reducer
 });
 
 export default rootReducer;
